c082440325cdb7937a33daa54c7066b71a2de99e
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-08-29 16:46:10 -0300

    update icon colors

--- components/actions/EditButton.tsx
+++ components/actions/EditButton.tsx
@@ -11,11 +11,11 @@ type EditButtonProps = {
 export const EditButton: React.FC<EditButtonProps> = ({ onEdit, title }) => {
   return (
     <span
-      className="absolute right-14 dark:hover:bg-zinc-800 hover:bg-zinc-300 p-2 rounded-full"
+      className="absolute right-14 dark:hover:bg-zinc-800 hover:bg-zinc-300 p-2 rounded-full cursor-pointer"
       title={`Editar ${title}`}
       onClick={onEdit}
     >
-      <Pencil className="w-[18px] h-[18px] cursor-pointer text-zinc-600 dark:text-zinc-400" />
+      <Pencil className="w-[18px] h-[18px] text-zinc-600 dark:text-zinc-400" />
     </span>
   );
 };
