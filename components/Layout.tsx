90281f034a28f824ea4e9fc2028411b9034ce74c
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-09-12 19:03:09 -0300

    feat: detect user to show aside panel

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,47 +12,49 @@ export default async function SideBarLayout({
 }) {
   return (
     <>
-      <aside className="hidden lg:block lg:w-[260px] xl:w-[300px] top-16 sticky h-screen border-r border-foreground/10 p-6">
-        <ul className="space-y-4 p-0 m-0 list-none">
-          {[
-            {
-              name: "Inicio",
-              url: "/",
-              icon: <Home className="w-5 h-5 inline mr-6" />,
-            },
-            {
-              name: "Notificaciones",
-              url: userProfile !== null ? "/notifications" : "/login",
-              icon: <Bell className="w-5 h-5 inline mr-6" />,
-            },
-            {
-              name: "Mensajes",
-              url: "/login",
-              icon: <Mail className="w-5 h-5 inline mr-6" />,
-            },
-            {
-              name: "Perfil",
-              url: userProfile !== null ? `/${userProfile}` : "/login",
-              icon: <User2 className="w-5 h-5 inline mr-6" />,
-            },
-          ].map((list) => (
+      {userProfile ? (
+        <aside className="hidden lg:block lg:w-[260px] xl:w-[300px] top-16 sticky h-screen border-r border-foreground/10 p-6">
+          <ul className="space-y-4 p-0 m-0 list-none">
+            {[
+              {
+                name: "Inicio",
+                url: "/",
+                icon: <Home className="w-5 h-5 inline mr-6" />,
+              },
+              {
+                name: "Notificaciones",
+                url: userProfile !== null ? "/notifications" : "/login",
+                icon: <Bell className="w-5 h-5 inline mr-6" />,
+              },
+              {
+                name: "Mensajes",
+                url: "/login",
+                icon: <Mail className="w-5 h-5 inline mr-6" />,
+              },
+              {
+                name: "Perfil",
+                url: userProfile !== null ? `/${userProfile}` : "/login",
+                icon: <User2 className="w-5 h-5 inline mr-6" />,
+              },
+            ].map((list) => (
+              <li className="flex items-center hover:bg-btn-background-hover dark:hover:bg-zinc-800 rounded-lg">
+                <Link
+                  href={list.url}
+                  className={`text-xl font-medium text-foreground flex items-center w-full p-2 ${
+                    list.name === "Mensajes" ? "cursor-not-allowed" : ""
+                  }`}
+                >
+                  {list.icon}
+                  {list.name}
+                </Link>
+              </li>
+            ))}
             <li className="flex items-center hover:bg-btn-background-hover dark:hover:bg-zinc-800 rounded-lg">
-              <Link
-                href={list.url}
-                className={`text-xl font-medium text-foreground flex items-center w-full p-2 ${
-                  list.name === "Mensajes" ? "cursor-not-allowed" : ""
-                }`}
-              >
-                {list.icon}
-                {list.name}
-              </Link>
+              <Share />
             </li>
-          ))}
-          <li className="flex items-center hover:bg-btn-background-hover dark:hover:bg-zinc-800 rounded-lg">
-            <Share />
-          </li>
-        </ul>
-      </aside>
+          </ul>
+        </aside>
+      ) : null}
       <main className="flex-1 flex flex-col justify-center mx-auto">
         {children}
       </main>
